51cb0ef4e61779c6680ad177437c712686029a20
LuisHouse <gabrielcasas1984@gmail.com>
2024-08-27 17:47:13 -0500

    Add files via upload

--- scrip.js
+++ scrip.js
@@ -1,29 +1,42 @@
- var botonEncriptar = document.querySelector(".btn-encriptar");
- var botonDesencriptar = document.querySelector(".btn-desencriptar");
- var botonMuneico = document.querySelector(".munieco");
- var contenedor = document.querySelector(".contenedor-parrafo");
- var resultado = document.querySelector(".texto-resultado");
+const emptyContent = document.getElementById("emptyContent");
+const resultContainer = document.getElementById("result");
+const resultField = document.getElementById("resultField");
 
- botonEncriptar.onclick = encriptar;
- botonDesencriptar.onclick = desencriptar;
+const input = document.getElementById("user-input");
 
- function encriptar(){
-    ocultarAdelante();
-    var cajatexto = recuperarTexto()
-    resultado.textContent = encriptarTexto(cajatexto);
+const btnEncrypt = document.querySelector(".btn-encriptar");
+btnEncrypt.addEventListener("click", () => {
+  emptyContent.style.display = "none";
+  resultContainer.style.display = "flex";
 
- }
+  resultField.textContent = encrypt(input.value);
+  input.value = "";
+});
 
- function desencriptar(){
-    ocultarAdelante();
-    var cajatexto = recuperarTexto()
-    resultado.textContent = desencriptarTexto(cajatexto);
- }
+const btnDecrypt = document.querySelector(".btn-desencriptar");
+btnDecrypt.addEventListener("click", () => {
+  emptyContent.style.display = "none";
+  resultContainer.style.display = "flex";
 
- function ocultarAdelante(){
-   botonMuneico.classList.add("ocultar");
-   contenedor.classList.add("ocultar");
+  resultField.textContent = decrypt(input.value);
+  input.value = "";
+});
 
- }
+function encrypt(str) {
+  const characters = str.split("");
+  const newValue = characters.map((c) => c.charCodeAt(0)).join("-");
+  console.log({ str, characters, newValue });
+  return newValue;
+  //   return str
+  //     .replaceAll("e", "23-")
+  //     .replaceAll("s", "52-")
+  //     .split("")
+  //     .reverse()
+  //     .join("");
+}
 
- 
+function decrypt(str) {
+  const characters = str.split("-");
+  const newValue = String.fromCharCode(...characters);
+  return newValue;
+}
